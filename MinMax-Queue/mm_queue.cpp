bde992249feb6cb5aecad2741b97cd4c2b65e32b
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2020-08-05 02:48:59 +0800

    min max queue

---
+++ MinMax-Queue/mm_queue.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MMQueue {
+    deque<vector<int>> mnq, mxq;
+    int len = 0;
+
+    void push(int v) {
+        len++;
+        int c = 1;
+        while(!mnq.empty() && v < mnq.back()[0]){
+            c += mnq.back()[1];
+            mnq.pop_back();
+        }
+        mnq.push_back({v,c});
+        c = 1;
+        while(!mxq.empty() && v > mxq.back()[0]){
+            c += mxq.back()[1];
+            mxq.pop_back();
+        }
+        mxq.push_back({v,c});
+    }
+    void pop() {
+        len--;
+        if(--mnq.front()[1] <=0) mnq.pop_front();
+        if(--mxq.front()[1] <=0) mxq.pop_front();
+    }
+    int min() {
+        return mnq.empty() ? 0 : mnq.front()[0];
+    }
+    int max() {
+        return mxq.empty() ? 0 : mxq.front()[0];
+    }
+};
