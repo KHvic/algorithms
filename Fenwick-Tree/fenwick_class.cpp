650e91834df7fe7674c4f83b0beb753152d23851
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2020-09-06 20:31:14 +0800

    refine fenwick & segment tree

--- Fenwick-Tree/fenwick_class.cpp
+++ Fenwick-Tree/fenwick_class.cpp
@@ -8,25 +8,27 @@ public:
         ft.assign(n + 1, 0);
     }
     int rsq(int b) {
+        b++;
         int sum = 0;
         for (; b; b -= (b&-b)) sum += ft[b];
         return sum;
     }
     int rsq(int a, int b) {
-        return rsq(b) - (a == 1 ? 0 : rsq(a - 1));
+        return rsq(b) - rsq(a-1);
     }
     void adjust(int k, int v) {
+        k++;
         for (; k < (int)ft.size(); k += (k&-k)) ft[k] += v;
     }
 };
 
 int main()
 {
-    int f[] = { 2,4,5,5,6,6,6,7,7,8,9 }; // m = 11 scores
-    FenwickTree ft(10); // declare a Fenwick Tree for range [1..10]
+    int f[] = { 0,2,4,5,5,6,6,6,7,7,8,9 }; // m = 11 scores
+    FenwickTree ft(11); // declare a Fenwick Tree for range [0..11]
     // insert these scores manually one by one into an empty Fenwick Tree
     for (int i = 0; i < 11; i++) ft.adjust(f[i], 1); // this is O(k log n)
-    // note that this fenwick tree can't query 0, because it index of tree start from 1
+    printf("%d\n", ft.rsq(0, 2)); // 2
     printf("%d\n", ft.rsq(1, 1)); // 0 => ft[1] = 0
     printf("%d\n", ft.rsq(1, 2)); // 1 => ft[2] = 1
     printf("%d\n", ft.rsq(1, 6)); // 7 => ft[6] + ft[4] = 5 + 2 = 7
---
+++ Segment-Tree/segment_class.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+struct ST{
+public:
+    vector<ll> segment;
+    int len;
+    ST(int n): len(n){
+        segment.assign(2*n,0);
+    }
+
+    ST(vector<ll>& arr): ST(arr.size()) {
+        for(int i=0;i<len;i++) segment[i+len] = arr[i];
+        for(int i=len-1;i>0;i--) segment[i] = segment[i*2]+segment[i*2+1];
+    }
+
+    void update(int i, ll val){
+        segment[len+i] = val;
+        for(i=(len+i)>>1;i>0;i>>=1) segment[i] = segment[i*2]+segment[i*2+1];
+    }
+
+    ll query(int l, int r){
+        ll res = 0;
+        for(l+=len, r+=len; l<=r; l >>= 1, r >>= 1){
+          if((l&1) == 1) res += segment[l++];
+          if((r&1) == 0)  res += segment[r--];
+        }
+        return res;
+    }
+};
+
+int main() {
+    vector<ll> arr = {1,3,5};
+    ST st(arr);
+    printf("%lld\n", st.query(0, 1)); // 4
+    st.update(1, 2);
+    printf("%lld\n", st.query(0, 1)); // 3
+    printf("%lld\n", st.query(0, 2)); // 8
+}
