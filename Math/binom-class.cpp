042d1b4a3e097d436d0d55363f07db42bbd0a825
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2020-11-24 22:08:09 +0800

    binom struct

---
+++ Math/binom-class.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+struct Binom {
+    ll mod = 1e9+7;
+    int n;
+    vector<ll> fac, inv;
+    Binom(int n): n(n) {
+        fac = inv = vector<ll>(n+1, 1);
+        for(int i=1;i<=n;i++) fac[i] = fac[i-1] * i % mod;
+        inv[n] = powe(fac[n], mod-2);
+        for(int i=n-1;i>=0;i--) inv[i] = inv[i+1] * (i+1) % mod;
+    }
+
+    ll choose(int n, int r) {
+        if(r>n || r<0) return 0;
+        return fac[n] * inv[r] % mod * inv[n-r] % mod;
+    }
+
+    ll permute(int n, int r) {
+        if(r>n || r<0) return 0;
+        return fac[n] * inv[n-r] % mod;
+    }
+
+    ll powe(ll x, ll e) {
+        ll r = 1;
+        while (e) {
+            if (e & 1) r = r * x % mod;
+            x = x * x % mod;
+            e >>= 1;
+        }
+        return r;
+    }
+};
