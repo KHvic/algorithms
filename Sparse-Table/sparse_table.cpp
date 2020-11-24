055871c00e008e72f25aec2f913b671ab4d03a78
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2020-11-25 02:06:00 +0800

    refine sparse table into struct

--- Sparse-Table/sparse_table.cpp
+++ Sparse-Table/sparse_table.cpp
@@ -1,44 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 3*1e5 + 10;
-const int MAXP = log2(MAXN) + 10;
-int min_table[MAXP][MAXN];
-int a[MAXN];
-int numlog[MAXN];
-int n;
+struct RMQ {
+    int n;
+    vector<int> numlog;
+    vector<vector<int>> min_table;
+    RMQ(vector<int>& vals):n(vals.size()) {
+        numlog = vector<int>(n+1);
+        numlog[1] = 0;
+        for(int i=2;i<=n;i++) numlog[i] = numlog[i/2] + 1;
 
-
-void init() {
-    numlog[1] = 0;
-    for(int i=2;i<=n;i++) numlog[i] = numlog[i/2] + 1;
-
-    for(int i=0;i<=numlog[n];i++) {
-        int skiprange = 1<<i;
-        for(int j=0;j+skiprange<=n;j++) {
-            if(i==0) {
-                min_table[i][j] = a[j];
-                continue;
+        min_table = vector<vector<int>>(numlog[n]+1, vector<int>(n+1));
+        for(int i=0;i<=numlog[n];i++) {
+            int skiprange = 1<<i;
+            for(int j=0;j+skiprange<=n;j++) {
+                if(i==0) {
+                    min_table[i][j] = vals[j];
+                    continue;
+                }
+                min_table[i][j] = operate(min_table[i-1][j], min_table[i-1][j+(skiprange>>1)]);
             }
-            min_table[i][j] = min(min_table[i-1][j], min_table[i-1][j+(skiprange>>1)]);
         }
     }
-}
 
-int query(int low, int high) {
-    int range = high-low+1;
-    int p = numlog[range];
-    int mn = min(min_table[p][low], min_table[p][high-(1<<p)+1]);
-    return mn;
-}
+    int query(int low, int high) {
+        int range = high-low+1;
+        int p = numlog[range];
+        int mn = operate(min_table[p][low], min_table[p][high-(1<<p)+1]);
+        return mn;
+    }
+
+    int operate(int a, int b) {
+        return min(a, b);
+    }
+};
 
 
 int main() {
     vector<int> tmp = {4,3,2,1};
-    n = tmp.size();
-    copy(tmp.begin(), tmp.end(), a);
-    init();
-    cout << query(0, 3) << "\n"; // 1
-    cout << query(0, 1) << "\n"; // 3
-    cout << query(1, 2) << "\n"; // 2
+    RMQ rmq(tmp);
+    cout << rmq.query(0, 3) << "\n"; // 1
+    cout << rmq.query(0, 1) << "\n"; // 3
+    cout << rmq.query(1, 2) << "\n"; // 2
 }
