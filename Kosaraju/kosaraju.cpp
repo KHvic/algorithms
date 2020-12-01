7e876a2754d3cdf7d8e8d58e64eaa6b059fd2bb9
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2020-12-01 23:57:25 +0800

    change kosaraju to struct format

--- Kosaraju/kosaraju.cpp
+++ Kosaraju/kosaraju.cpp
@@ -1,60 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_map<int,vector<int>> graph;
-unordered_map<int,vector<int>> rgraph;
-unordered_set<int> visited;
-stack<int> topdown;
-unordered_map<int,int> represent;
-int numSCC = 0;
-int n;
+struct Kosaraju {
+    vector<vector<int>> graph, rgraph;
+    vector<int> represent;
+    vector<bool> visited;
+    stack<int> topdown;
+    int numSCC;
+    int n;
 
-void dfs1(int idx) {
-    if(visited.count(idx)) return;
-    visited.insert(idx);
-    for(int next : graph[idx])
-        dfs1(next);
-    topdown.push(idx);
-}
-
-void dfs2(int idx, int rep) {
-    if(visited.count(idx)) return;
-    visited.insert(idx);
-    represent[idx] = rep;
-    for(int next : rgraph[idx])
-        dfs2(next, rep);
-}
-
-void kosaraju() {
-    // step1: do dfs on normal graph to get stack topdown ordering
-    for(int i=0;i<n;i++) {
-        dfs1(i);
+    Kosaraju(int N): n(N) {
+        graph = rgraph = vector<vector<int>>(n);
     }
-    visited.clear();
-    // step2: find representative for each node using reverse graph and topdown stack
-    while(!topdown.empty()) {
-        int cur = topdown.top(); topdown.pop();
-        if(visited.count(cur)) continue;
-        dfs2(cur, cur);
-        numSCC++;
-    }
-}
 
-void constructGraph(vector<vector<int>>& edges, int s) {
-    n = s;
-    for(auto edge : edges) {
-        graph[edge[0]].push_back(edge[1]);
-        rgraph[edge[1]].push_back(edge[0]);
+    void dfs1(int idx) {
+        if(visited[idx]) return;
+        visited[idx] = true;
+        for(int next : graph[idx]) dfs1(next);
+        topdown.push(idx);
     }
-}
+
+    void dfs2(int idx, int rep) {
+        if(visited[idx]) return;
+        visited[idx] = true;
+        represent[idx] = rep;
+        for(int next : rgraph[idx]) dfs2(next, rep);
+    }
+
+    int start() {
+        numSCC = 0;
+        represent = vector<int>(n);
+        visited = vector<bool>(n);
+        // step1: do dfs on normal graph to get stack topdown ordering
+        for(int i=0;i<n;i++) dfs1(i);
+
+        // step2: find representative for each node using reverse graph and topdown stack
+        visited.assign(n, false);
+        while(!topdown.empty()) {
+            int cur = topdown.top(); topdown.pop();
+            if(visited[cur]) continue;
+            dfs2(cur, cur);
+            numSCC++;
+        }
+        return numSCC;
+    }
+
+    void addEdge(int a, int b) {
+        graph[a].push_back(b);
+        rgraph[b].push_back(a);
+    }
+};
 
 int main() {
     // expect 3 SCC from this graph
+    Kosaraju ko(6);
     vector<vector<int>> edges = {{0,1},{1,2},{2,4},{4,5},{5,4},{3,1},{2,3}};
-    constructGraph(edges, 6);
-    kosaraju();
-    printf("Number of SCC: %d\n", numSCC);
-    for(auto p : represent) {
-        printf("Node %d, representative node %d\n", p.first, p.second);
+    for(auto& edge : edges) ko.addEdge(edge[0], edge[1]);
+
+    ko.start();
+    printf("Number of SCC: %d\n", ko.numSCC);
+    for(int i=0; i<ko.n; i++) {
+        printf("Node %d, representative node %d\n", i, ko.represent[i]);
     }
 }
