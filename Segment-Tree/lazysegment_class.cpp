80953bd84e69203c7b361a69b8fd1c278eb89c71
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2021-04-26 22:20:02 +0800

    refine lazy segment tree

--- Segment-Tree/lazysegment_class.cpp
+++ Segment-Tree/lazysegment_class.cpp
@@ -1,64 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 // sum segment tree
-struct ST {
+class ST {
     int n;
-    vector<int> segment, lazy;
+    const ll LAZY = 0, DEFAULT = 0;
+    vector<ll> segment, lazy;
+public:
+    ST() {}
+
     ST(int n):n(n){
-        segment.assign(4*n, 0);
-        lazy.assign(4*n, 0);
+        segment.assign(4*n, DEFAULT);
+        lazy.assign(4*n, LAZY);
     }
 
+    ST(vector<ll>& vals): ST(vals.size()) {
+        for(int i = 0; i < n; ++i) update(i, i, vals[i]);
+    }
+
+    void update(int qlow, int qhi, ll val) {
+        update(0, 0, n-1, qlow, qhi, val);
+    }
+
+    ll query(int qlow, int qhi) {
+        return query(0, 0, n-1, qlow, qhi);
+    }
+
+private:
     void updateLazy(int idx, int low, int high) {
-        if(lazy[idx] && low != high) {
+        if(lazy[idx] != LAZY && low != high) {
             lazy[idx*2+1] += lazy[idx];
             lazy[idx*2+2] += lazy[idx];
         }
         // update logic
-        if(lazy[idx]) {
+        if(lazy[idx] != LAZY) {
             segment[idx] += (high-low+1)*lazy[idx];
         }
-        lazy[idx] = 0;
+        lazy[idx] = LAZY;
     }
 
-    void update(int idx, int low, int hi, int qlow, int qhi, int val) {
+    void update(int idx, int low, int hi, int qlow, int qhi, ll val) {
         updateLazy(idx, low, hi);
-        if(low>qhi || hi<qlow) return;
-        else if(low>=qlow && hi<=qhi) {
+        if(low > qhi || hi < qlow) return;
+        else if(low >= qlow && hi <= qhi) {
             lazy[idx] = val;
             updateLazy(idx, low, hi);
         } else {
-            int mid = (low+hi) >> 1;
+            int mid = (low + hi) >> 1;
             update(idx*2+1, low, mid, qlow, qhi, val);
             update(idx*2+2, mid+1, hi, qlow, qhi, val);
-            segment[idx] = segment[idx*2+1] + segment[idx*2+2];
+            segment[idx] = operate(segment[idx*2+1], segment[idx*2+2]);
         }
     }
 
-    void update(int qlow, int qhi, int val) {
-        update(0, 0, n-1, qlow, qhi, val);
-    }
-
-    int query(int idx, int low, int hi, int qlow, int qhi) {
+    ll query(int idx, int low, int hi, int qlow, int qhi) {
         updateLazy(idx, low, hi);
-        if(low>qhi || hi<qlow) return 0;
-        else if(low>=qlow && hi<=qhi) return segment[idx];
+        if(low > qhi || hi < qlow) return DEFAULT;
+        else if(low >= qlow && hi <= qhi) return segment[idx];
         int mid = (low+hi) >> 1;
-        return query(idx*2+1, low, mid, qlow, qhi) + query(idx*2+2, mid+1, hi, qlow, qhi);
+        return operate(query(idx*2+1, low, mid, qlow, qhi), query(idx*2+2, mid+1, hi, qlow, qhi));
     }
 
-    int query(int qlow, int qhi) {
-        return query(0, 0, n-1, qlow, qhi);
+    ll operate(ll a, ll b) {
+        return a + b;
     }
 };
 
 int main(){
-    vector<int> test = {1, 2, 3, 4, 5, 10};
-    ST st(test.size());
-    for(int i=0;i<test.size();i++) st.update(i, i, test[i]);
-    printf("%d\n", st.query(1, 2)); // 5
-    printf("%d\n", st.query(0, 5)); // 25
+    vector<ll> test = {1, 2, 3, 4, 5, 10};
+    ST st(test);
+    printf("%lld\n", st.query(1, 2)); // 5
+    printf("%lld\n", st.query(0, 5)); // 25
     st.update(0, 2, 2); // adjust all by +2
-    printf("%d\n", st.query(0, 3)); // 16 (3, 4, 5, 4)
+    printf("%lld\n", st.query(0, 3)); // 16 (3, 4, 5, 4)
 }
