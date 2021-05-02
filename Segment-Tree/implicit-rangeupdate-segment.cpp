8260803598c35eaa2cac81ac81304fc01e2069a3
Victor Goh Ka Hian <kahiangohvictor@gmail.com>
2021-05-02 13:58:53 +0800

    implicit st refinement

--- Segment-Tree/implicit-rangeupdate-segment.cpp
+++ Segment-Tree/implicit-rangeupdate-segment.cpp
@@ -5,26 +5,34 @@ using namespace std;
 // allow range update but only single point query
 // can support max, min, gcd
 // one node represent a range update itself, doesn't contain child values in constrast to typically segment tree
-
 struct Node {
     int val;
     Node *l, *r;
     Node(int v):val(v), l(0), r(0) {}
 };
 
-struct ST {
+class ST {
     Node* root;
     int n;
     const int DEFAULT = 0;
+public:
     ST(int n):n(n) {
         root = new Node(DEFAULT);
     }
 
+    void update(int qlow, int qhi, int val) {
+        update(root, 0, n-1, qlow, qhi, val);
+    }
+
+    int query(int idx) {
+        return query(root, 0, n-1, idx);
+    }
+private:
     void update(Node* node, int low, int hi, int qlow, int qhi, int val) {
         if(!node || low>qhi || hi<qlow) return;
         if(low>=qlow && hi<=qhi) {
             // update logic
-            node->val = max(val, node->val);
+            node->val = operate(val, node->val);
         } else {
             if(!node->l) {
                 node->l = new Node(DEFAULT);
@@ -36,17 +44,13 @@ struct ST {
         }
     }
 
-    void update(int qlow, int qhi, int val) {
-        update(root, 0, n-1, qlow, qhi, val);
-    }
-
     int query(Node* node, int low, int hi, int idx) {
         if(!node || low>idx || hi<idx) return DEFAULT;
         int mid = (low+hi) >> 1;
-        return max(max(query(node->l, low, mid, idx), query(node->r, mid+1, hi, idx)), node->val);
+        return operate(operate(query(node->l, low, mid, idx), query(node->r, mid+1, hi, idx)), node->val);
     }
 
-    int query(int idx) {
-        return query(root, 0, n-1, idx);
+    int operate(int a, int b) {
+        return max(a, b);
     }
 };
